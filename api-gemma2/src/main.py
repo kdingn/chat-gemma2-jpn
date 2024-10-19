2d587ac379ab25cb6bced9acf2b2a4dc944c5983
kdingn <koukou3335@gmail.com>
2024-10-19 23:48:00 +0900

    implement simple post request

--- api-gemma2/src/main.py
+++ api-gemma2/src/main.py
@@ -8,16 +8,21 @@ pipe = pipeline(
     model="../model",
     model_kwargs={"torch_dtype": torch.bfloat16},
 )
-messages = [
-    {
-        "role": "user",
-        "content": "マシーンラーニングについての詩を書いてください。",
-    },
-]
-outputs = pipe(messages, return_full_text=False, max_new_tokens=256)
-assistant_response = outputs[0]["generated_text"].strip()
 
 
 @app.get("/")
 async def root():
-    return {"message": assistant_response}
+    return {"response": "Hello World!"}
+
+
+@app.post("/chat")
+async def chat(message):
+    messages = [
+        {
+            "role": "user",
+            "content": message,
+        },
+    ]
+    outputs = pipe(messages, return_full_text=False, max_new_tokens=256)
+    assistant_response = outputs[0]["generated_text"].strip()
+    return {"response": assistant_response}
