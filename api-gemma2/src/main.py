dcc2ef67be51c23a5ed5c867e13b912aee1fb759
kdingn <koukou3335@gmail.com>
2024-10-19 12:11:05 +0900

    implement simple api

--- api-gemma2/src/main.py
+++ api-gemma2/src/main.py
@@ -1,8 +1,23 @@
+import torch
 from fastapi import FastAPI
+from transformers import pipeline
 
 app = FastAPI(root_path="/api")
+pipe = pipeline(
+    "text-generation",
+    model="../model",
+    model_kwargs={"torch_dtype": torch.bfloat16},
+)
+messages = [
+    {
+        "role": "user",
+        "content": "マシーンラーニングについての詩を書いてください。",
+    },
+]
+outputs = pipe(messages, return_full_text=False, max_new_tokens=256)
+assistant_response = outputs[0]["generated_text"].strip()
 
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": assistant_response}
